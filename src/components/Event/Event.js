7389219ee42dca22960283eab4d3574a6ab0dc8b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-13 12:07:56 +0600

    some design changed

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function Event({event}) {
   return (
-    <div className="col-md-4 mb-2">
-            <div class="card">
-                <img src={event.image} class="card-img-top" alt="..."/>
+    <div className="col-md-4 mb-2 ">
+            <div class="card shadow">
+                <img src={event.image} class="card-img-top p-1" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">{event.eventName}</h5>
                     <Link href="#" class="btn btn-primary">Click For Book</Link>
