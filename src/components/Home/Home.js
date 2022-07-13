8f052158c5bc11445f0226ce41fba9d7bbc3b247
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-13 11:52:15 +0600

    Fetch data completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,18 @@
 import React from 'react'
 import { faker } from '@faker-js/faker';
-import { events } from '../FakeData/FakeData';
 import Event from '../Event/Event';
 
 
 export default function Home() {
-  console.log(events);
-  const handleEvents = () => {
-    fetch('http://localhost:5000/postEvents', {
-    method: 'POST',
-    body: JSON.stringify(events),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
+  const [events, setEvents] = React.useState([]);
+  React.useEffect(() => {
+    fetch('http://localhost:5000/getEvents')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setEvents(data);
     })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          console.log(data)
-        } else {
-          console.log("No data submitted")
-        }
-      });
-  }
+  }, []);
 
 
   return (
@@ -31,7 +21,6 @@ export default function Home() {
         {
           events.map(event => <Event key={event.id} event={event}></Event>)
         }
-        <button onClick={handleEvents}>Post All Events</button>
       </div>
     </div>
   )
