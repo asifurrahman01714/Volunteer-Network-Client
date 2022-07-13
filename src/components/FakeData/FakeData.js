393e0dd641877012cc307d3152c52844d1a24f6b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-13 10:31:25 +0600

    Generate fake data

---
+++ src/components/FakeData/FakeData.js
@@ -0,0 +1,31 @@
+export  const events = [{
+    "eventName": "Child Support",
+    "image": "https://robohash.org/eligendiestaut.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Food Charity",
+    "image": "https://robohash.org/enimlaudantiumdolor.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Refuse Shelter",
+    "image": "https://robohash.org/quiavoluptatequi.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Clothing Camp",
+    "image": "https://robohash.org/voluptatesofficiafuga.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Food Camp",
+    "image": "https://robohash.org/atquidemdeserunt.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Good Education",
+    "image": "https://robohash.org/abnihildolor.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Pure Water For Children",
+    "image": "https://robohash.org/optiomollitiaest.png?size=50x50&set=set1"
+  }, {
+    "eventName": "New Books For Children",
+    "image": "https://robohash.org/aspernaturetet.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Study Group",
+    "image": "https://robohash.org/doloresteos.png?size=50x50&set=set1"
+  }, {
+    "eventName": "Clean Your Local Park",
+    "image": "https://robohash.org/earumutminus.png?size=50x50&set=set1"
+  }]
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { faker } from '@faker-js/faker';
+import { events } from '../FakeData/FakeData';
+
 
 export default function Home() {
+  console.log(events);
   return (
     <div>Home</div>
   )
