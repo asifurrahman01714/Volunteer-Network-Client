1713799e7dd60ed06d8702031b6140d7416e684c
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-13 16:30:57 +0600

    data from react hook form is done

--- src/components/AddEvents/AddEvents.js
+++ src/components/AddEvents/AddEvents.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { UserContext } from "../../App";
 
 export default function AddEvents() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-
-  console.log(watch("example")); // watch input value by passing the name of it
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  
+  const onSubmit = (data) =>{
+    fetch('http://localhost:5000/addEvent', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  };
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
+    <>
+        <div className="container">
+            <div className="row">
+                <div className="col-4 offset-4 border shadow p-5">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h5 className="text-center text-primary mb-3">Add Your Favorite Event</h5>
+                        <input value={loggedInUser.name} {...register("Name")} className="form-control"/> <br />
+                        <input placeholder="Enter Your Event Name" {...register("Event Name", { required: true })} className="form-control"/> <br />
+                        {errors.exampleRequired && <span>This field is required</span>}
+                        
+                        <input type="submit" className="form-control"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
   );
 }
