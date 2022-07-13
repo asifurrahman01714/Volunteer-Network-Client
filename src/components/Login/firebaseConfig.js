20417a83c829820b249e9869bf5e89dfab914833
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-13 15:12:48 +0600

    Installed firebase

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAHaaiDOFhcc7jgpZ6ZqubpEFd3blZ5rfI",
+    authDomain: "volunteer-network-21c02.firebaseapp.com",
+    projectId: "volunteer-network-21c02",
+    storageBucket: "volunteer-network-21c02.appspot.com",
+    messagingSenderId: "460217483133",
+    appId: "1:460217483133:web:87e82e5517149cdc6a5614"
+  };
