05fad8e967fe8e79dd9f8dd2de89355a8a73372f
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-13 11:39:52 +0600

    Post data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,26 @@ import Event from '../Event/Event';
 
 export default function Home() {
   console.log(events);
+  const handleEvents = () => {
+    fetch('http://localhost:5000/postEvents', {
+    method: 'POST',
+    body: JSON.stringify(events),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+
   return (
     <div className="container">
       <div className='row'>
         {
           events.map(event => <Event key={event.id} event={event}></Event>)
         }
+        <button const={handleEvents}>Post All Events</button>
       </div>
     </div>
   )
